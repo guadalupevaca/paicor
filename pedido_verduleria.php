deb7417a76a2e1ee96921f696469ff3ef85a5ec4
yohanavaca <132697011+yohanavaca@users.noreply.github.com>
2023-11-21 14:41:53 -0300

    VERDULERIA- FECHA DE ENTREGA

--- pedido_verduleria.php
+++ pedido_verduleria.php
@@ -13,20 +13,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
     $fechaPedido = mysqli_real_escape_string($conexion, $_POST['fecha_pedido']);
     $papas = mysqli_real_escape_string($conexion, $_POST['papas']);
+    $fecha_papas = mysqli_real_escape_string($conexion, $_POST['fecha_papas']);
     $tomate = mysqli_real_escape_string($conexion, $_POST['tomate']);
+    $fecha_tomate = mysqli_real_escape_string($conexion, $_POST['fecha_tomate']);
     $cebolla = mysqli_real_escape_string($conexion, $_POST['cebolla']);
+    $fecha_cebolla = mysqli_real_escape_string($conexion, $_POST['fecha_cebolla']);
     $zanahoria = mysqli_real_escape_string($conexion, $_POST['zanahoria']);
+    $fecha_zanahoria = mysqli_real_escape_string($conexion, $_POST['fecha_zanahoria']);
     $choclo = mysqli_real_escape_string($conexion, $_POST['choclo']);
+    $fecha_choclo = mysqli_real_escape_string($conexion, $_POST['fecha_choclo']);
     $zapallitos = mysqli_real_escape_string($conexion, $_POST['zapallitos']);
+    $fecha_zapallitos = mysqli_real_escape_string($conexion, $_POST['fecha_zapallitos']);
     $calabacin = mysqli_real_escape_string($conexion, $_POST['calabacin']);
+    $fecha_calabacin = mysqli_real_escape_string($conexion, $_POST['fecha_calabacin']);
     $lechuga = mysqli_real_escape_string($conexion, $_POST['lechuga']);
+    $fecha_lechuga = mysqli_real_escape_string($conexion, $_POST['fecha_lechuga']);
     $acelga = mysqli_real_escape_string($conexion, $_POST['acelga']);
+    $fecha_acelga = mysqli_real_escape_string($conexion, $_POST['fecha_acelga']);
     $berenjena = mysqli_real_escape_string($conexion, $_POST['berenjena']);
+    $fecha_berenjena= mysqli_real_escape_string($conexion, $_POST['fecha_berenjena']);
     $espinaca = mysqli_real_escape_string($conexion, $_POST['espinaca']);
+    $fecha_espinaca = mysqli_real_escape_string($conexion, $_POST['fecha_espinaca']);
     $otrosProductos = mysqli_real_escape_string($conexion, $_POST['otros_productos']);
 
-    $sql = "INSERT INTO pedidos_verduras (nombre, fecha_pedido, papas, tomate, cebolla, zanahoria, choclo, zapallitos, calabacin, lechuga, acelga, berenjena, espinaca, otros_productos)
-            VALUES ('$nombre','$fechaPedido', '$papas', '$tomate', '$cebolla', '$zanahoria', '$choclo', '$zapallitos', '$calabacin', '$lechuga', '$acelga', '$berenjena', '$espinaca', '$otrosProductos')";
+    $sql = "INSERT INTO pedidos_verduras (nombre, fecha_pedido, papas, fecha_papas, tomate, fecha_tomate, cebolla, fecha_cebolla, zanahoria, fecha_zanahoria, choclo, fecha_choclo, zapallitos, fecha_zapallitos, calabacin, fecha_calabacin, lechuga, fecha_lechuga, acelga, fecha_acelga, berenjena, fecha_berenjena, espinaca, fecha_espinaca, otros_productos)
+    VALUES ('$nombre', '$fechaPedido', '$papas', '$fecha_papas', '$tomate', '$fecha_tomate', '$cebolla', '$fecha_cebolla', '$zanahoria', '$fecha_zanahoria', '$choclo', '$fecha_choclo', '$zapallitos', '$fecha_zapallitos', '$calabacin', '$fecha_calabacin', '$lechuga', '$fecha_lechuga', '$acelga', '$fecha_acelga', '$berenjena', '$fecha_berenjena', '$espinaca', '$fecha_espinaca', '$otrosProductos')";
+
 
     if (mysqli_query($conexion, $sql)) {
         echo '<div class="success-message">Pedido enviado.</div>';
--- verduleria.php
+++ verduleria.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="stylesheet" href="verduleria.css">
 
     <title>Pedido de Verdulería</title>
 </head>
@@ -36,69 +36,87 @@
     </select>
     <label for="acelga">Acelga:</label>
     <input type="text" name="acelga"><br>
+    <input type="date" name="fecha_acelga"><br>
 
     <label for="ajo">Ajo:</label>
     <input type="text" name="ajo"><br>
+    <input type="date" name="fecha_ajo"><br>
 
     <label for="bananas">Bananas:</label>
     <input type="text" name="bananas"><br>
+    <input type="date" name="fecha_bananas"><br>
 
     <label for="calabacin">Calabacin:</label>
     <input type="text" name="calabacin"><br>
-
-    <label for="acelga">acelga:</label>
-    <input type="text" name="acelga"><br>
+    <input type="date" name="fecha_calabacin"><br>
 
     <label for="espinaca">espinaca:</label>
     <input type="text" name="espinaca"><br>
+    <input type="date" name="fecha_espinaca"><br>
 
     <label for="lechuga">lechuga:</label>
     <input type="text" name="lechuga"><br>
+    <input type="date" name="fecha_lechuga"><br>
 
     <label for="cebolla">Cebolla:</label>
     <input type="text" name="cebolla"><br>
+    <input type="date" name="fecha_cebolla"><br>
 
     <label for="limon">Limón:</label>
     <input type="text" name="limon"><br>
+    <input type="date" name="fecha_limon"><br>
 
     <label for="mandarinas">Mandarinas:</label>
     <input type="text" name="mandarinas"><br>
+    <input type="date" name="fecha_mandarinas"><br>
 
     <label for="manzanas">Manzanas:</label>
     <input type="text" name="manzanas"><br>
+    <input type="date" name="fecha_manzanas"><br>
 
     <label for="naranjas">Naranjas:</label>
     <input type="text" name="naranjas"><br>
+    <input type="date" name="fecha_naranjas"><br>
 
     <label for="papas">Papas:</label>
     <input type="text" name="papas"><br>
+    <input type="date" name="fecha_papas"><br>
 
     <label for="pimientos">Pimientos:</label>
     <input type="text" name="pimientos"><br>
+    <input type="date" name="fecha_pimientos"><br>
 
     <label for="remolachas">Remolachas:</label>
     <input type="text" name="remolachas"><br>
+    <input type="date" name="fecha_remolachas"><br>
 
     <label for="tomate">Tomate:</label>
     <input type="text" name="tomate"><br>
+    <input type="date" name="fecha_tomate"><br>
 
     <label for="zanahoria">Zanahoria:</label>
     <input type="text" name="zanahoria"><br>
+    <input type="date" name="fecha_zanahoria"><br>
 
     <label for="zapallitos">Zapallitos:</label>
     <input type="text" name="zapallitos"><br>
+    <input type="date" name="fecha_zapallitos"><br>
 
     <label for="zapallos">Zapallos:</label>
     <input type="text" name="zapallos"><br>
+    <input type="date" name="fecha_zapallos"><br>
 
     <label for="choclo">choclo:</label>
     <input type="text" name="choclo"><br>
+    <input type="date" name="fecha_choclo"><br>
 
     <label for="berenjena">berenjena:</label>
     <input type="text" name="berenjena"><br>
+    <input type="date" name="fecha_berenjena"><br>
 
     <label for="cajon_estacion">Cajón de Estación:</label>
     <input type="text" name="cajon_estacion"><br>
+    <input type="date" name="fecha_cajon"><br>
 
     <label for="otros_productos">Otros Productos Adicionales:</label>
     <textarea name="otros_productos" rows="4" cols="50"></textarea><br>
